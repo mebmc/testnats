850ddad604c1d510d0dcd34ed3ac553cc93a5baa
Ben McDonald <ben@mebmc.uk>
2019-03-05 13:03:57 +0000

    Update scripts

--- scripts/siege.sh
+++ scripts/siege.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-siege -c5000 -t1s --content-type "application/json"	\
+siege -c50 -t15m --content-type "application/json"	\
 	'http://dev.local:8080/api/submit POST {"id": "1", "first_name": "alice", "last_name": "Smith", "email": "alice@example.com", "guess": "a"}'
